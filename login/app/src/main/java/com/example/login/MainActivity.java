f5a52dd0037d3ec2df3e5f1b7b243bade091f1d8
ifrah <ifrag@gmail.com>
2022-05-19 01:36:16 +0500

    login as admin

---
+++ login/app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText et_username, et_password;
+    Button btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Login();
+    }
+
+    void Login(){
+        et_username = (EditText)findViewById(R.id.et_username);
+        et_password = (EditText)findViewById(R.id.et_password);
+        btn_login = (Button)findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(et_username.getText().toString().equals("admin") && et_password.getText().toString().equals("admin")){
+
+                    Intent intent = new Intent(MainActivity.this, User.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this, "Username or Password is incorrect", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
